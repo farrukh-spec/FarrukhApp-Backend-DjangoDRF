7d595283b4cb5e4fb52b0cda856b4da4c1bfc666
farrukh-spec <farrukh@astutesoftwares.com>
2026-05-13 17:13:47 +0500

    updated the allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -33,8 +33,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['farrukhapp-django-drf.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
