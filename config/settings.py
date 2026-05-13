85ead8b6c368a84846138d6f84586165456f5292
farrukh-spec <farrukh@astutesoftwares.com>
2026-05-13 18:27:16 +0500

    new deploymnet

--- config/settings.py
+++ config/settings.py
@@ -32,9 +32,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-ALLOWED_HOSTS = ['farrukhapp-django-drf.onrender.com', 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['farrukhapp-django-drf.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
